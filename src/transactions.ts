851cbb01c138314554250468b61d6ae737da1c32
Falghannam <155424719+FatmaAlghannam@users.noreply.github.com>
2025-12-10 19:54:14 +0300

    DONE

--- src/transactions.ts
+++ src/transactions.ts
@@ -2,10 +2,7 @@
 type TransactionType = "income" | "expense";
 type Transaction = [TransactionType, number];
 
-/**
- * You can use this array to
- * manually test your functions
- */
+
 const transactions: Transaction[] = [
   ["income", 1000],
   ["income", 1500],
@@ -14,76 +11,71 @@ const transactions: Transaction[] = [
   ["income", 700],
 ];
 
-// `filterIncomeTransactions` function that:
-// - Accepts a "transactions" parameter of type "Transaction[]".
-// - Returns a new array containing only the income transactions.
-// example:
-// filterIncomeTransactions(transactions); // => [["income", 1000], ["income", 1500], ["income", 700]]
+//Create a new array containing only the income transactions.
+// Output 1) [["income", 1000], ["income", 1500], ["income", 700]]
+
 function filterIncomeTransactions(transactions: Transaction[]): Transaction[] {
-  // write your code here...
-
-  return []; // replace empty array with what you see is fit
+ 
+  return transactions.filter(transaction => transaction[0] === "income"); 
 }
+console.log(filterIncomeTransactions(transactions));
 
-// `filterExpenseTransactions` function that:
-// - Accepts a "transactions" parameter of type "Transaction[]".
-// - Return a new array containing only the expense transactions.
-// example:
-// filterExpenseTransactions(transactions); // => [["expense", 500], ["expense", 300]]
+
+
 function filterExpenseTransactions(transactions: Transaction[]): Transaction[] {
-  // write your code here...
+  const filterExpenseTransactions = transactions.filter(transaction => transaction[0] === "expense");
 
-  return []; // replace empty array with what you see is fit
+  return filterExpenseTransactions.filter(transactions => transactions[0] === "expense"); 
+
 }
+console.log(filterExpenseTransactions(transactions));
 
-// `calculateTotalIncome` function that:
-// - Accepts a "transactions" parameter of type "Transaction[]".
-// - calculates the total income and returns it.
-// example:
-// calculateTotalIncome(transactions); // => 3200  (1000 + 1500 + 700)
+
+
 function calculateTotalIncome(transactions: Transaction[]): number {
-  // write your code here...
 
-  return -1; // replace -1 with what you see is fit
+  const totalIncome = transactions
+  .filter(t => t[0] === "income")
+  .reduce((sum, t) => sum + t[1], 0);
+  
+  return totalIncome; 
 }
+console.log(calculateTotalIncome(transactions));
 
-// `calculateTotalExpenses` function that:
-// - Accepts a "transactions" parameter of type "Transaction[]".
-// - calculates the total expense and returns it.
-// example:
-// calculateTotalExpenses(transactions); // => 800  (500 + 300)
+
 function calculateTotalExpenses(transactions: Transaction[]): number {
-  // write your code here...
 
-  return -1; // replace -1 with what you see is fit
+  const totalExpenses = transactions
+  .filter(t => t[0] === "expense")
+  .reduce((sum, t) => sum + t[1], 0);
+  
+  return totalExpenses; 
 }
+console.log(calculateTotalExpenses(transactions));
 
-// `calculateNetTotal` function that:
-// - Accepts a "transactions" parameter of type "Transaction[]".
-// - calculates the net total (total income - total expenses) and returns it.
-// example:
-// calculateNetTotal(transactions); // => 2400  (3200 - 800)
+
 function calculateNetTotal(transactions: Transaction[]): number {
-  // write your code here...
-
-  return -1; // replace -1 with what you see is fit
+  const totalIncome = transactions
+    .filter(t => t[0] === "income")
+    .reduce((sum, t) => sum + t[1], 0);
+  
+  const totalExpenses = transactions
+    .filter(t => t[0] === "expense")
+    .reduce((sum, t) => sum + t[1], 0);
+  
+  return totalIncome - totalExpenses;
 }
+console.log(calculateNetTotal(transactions));
 
-// `filterSignificantTransactions` function that:
-// - Accepts 2 parameters:
-// - a "transactions" parameter of type "Transaction[]".
-// - a "threshold" parameter of type "number"
-// - identifies and creates a new array with transactions (both incomes and expenses) above or equal to "threshold".
-// example:
-// filterSignificantTransactions(transactions, 1000); // =>  [["income", 1000], ["income", 1500]]
+
+
 function filterSignificantTransactions(
   transactions: Transaction[],
   threshold: number
 ): Transaction[] {
-  // write your code here...
-
-  return []; // replace empty array with what you see is fit
+  return transactions.filter((t: Transaction) => t[1] >= threshold);
 }
+console.log(filterSignificantTransactions(transactions, 1000));
 
 export {
   Transaction,
@@ -94,3 +86,4 @@ export {
   calculateNetTotal,
   filterSignificantTransactions,
 };
+
